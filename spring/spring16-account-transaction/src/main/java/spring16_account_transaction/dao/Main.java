6254644ba3541533505fef10cd9fce5cf3bcc526
ziming <zimingnee@163.com>
2020-05-16 22:25:01 +0800

    spring16-spring事物

---
+++ spring/spring16-account-transaction/src/main/java/spring16_account_transaction/dao/Main.java
@@ -0,0 +1,9 @@
+package spring16_account_transaction.dao;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("hello");
+        }
+    }
+}
